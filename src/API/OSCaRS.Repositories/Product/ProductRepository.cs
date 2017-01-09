df8183616df06b002aabb921fb9e93c0d74ae958
nicolaspierre1990 <nico_kestens@hotmail.com>
2017-01-09 16:00:25 +0100

    Added ProductRepository

---
+++ src/API/OSCaRS.Repositories/Product/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using OSCaRS.Core.Repository;
+using OSCaRS.Domain.Context;
+
+namespace OSCaRS.Repositories.Product
+{
+    public class ProductRepository : Repository<OSCaRS.Domain.Product>
+    {
+        public ProductRepository(OSCaRSContext context) : base(context)
+        {
+        }
+    }
+}
