85998bdfe02b620891d26b11ce93d038c61a9e09
nicolaspierre1990 <nico_kestens@hotmail.com>
2017-01-10 12:25:38 +0100

    add Travis.yml

--- src/API/OSCaRS.Tests/Product/ProductControllerTests.cs
+++ src/API/OSCaRS.Tests/Product/ProductControllerTests.cs
@@ -11,6 +11,8 @@ namespace OSCaRS.Tests.Product
 {
     public class ProductControllerTests : TestBase
     {
+        #region Add Tests
+
         [Fact]
         public async Task AddProductSuccesFull()
         {
@@ -27,5 +29,58 @@ namespace OSCaRS.Tests.Product
                 Assert.Equal(response.StatusCode, HttpStatusCode.Created);
             }
         }
+
+        [Fact]
+        public async Task AddProductShortDescMissing()
+        {
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(client.BaseAddress + PRODUCT_CONTROLLER_BASE),
+                Method = HttpMethod.Post,
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(new AddProductViewModel() { ShortDesc = null, Description = "Coca Cola", BasePrice = 1.8M }), Encoding.UTF8, JSON_MEDIA_TYPE)
+            };
+
+            using (HttpResponseMessage response = await client.SendAsync(request))
+            {
+                Assert.Equal(response.StatusCode, HttpStatusCode.Created);
+            }
+        }
+
+        [Fact]
+        public async Task AddProductDescMissing()
+        {
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(client.BaseAddress + PRODUCT_CONTROLLER_BASE),
+                Method = HttpMethod.Post,
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(new AddProductViewModel() { ShortDesc = "COLA", Description = null, BasePrice = 1.8M }), Encoding.UTF8, JSON_MEDIA_TYPE)
+            };
+
+            using (HttpResponseMessage response = await client.SendAsync(request))
+            {
+                Assert.Equal(response.StatusCode, HttpStatusCode.Created);
+            }
+        }
+
+        [Fact]
+        public async Task AddProductBasePriceZero()
+        {
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(client.BaseAddress + PRODUCT_CONTROLLER_BASE),
+                Method = HttpMethod.Post,
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(new AddProductViewModel() { ShortDesc = null, Description = "Coca Cola", BasePrice = 0 }), Encoding.UTF8, JSON_MEDIA_TYPE)
+            };
+
+            using (HttpResponseMessage response = await client.SendAsync(request))
+            {
+                Assert.Equal(response.StatusCode, HttpStatusCode.Created);
+            }
+        }
+
+        #endregion
     }
 }
